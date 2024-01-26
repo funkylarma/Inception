4f4a73b1075b208907987bb2a4e89c55b77b1d45
Adam Chamberlin <adam@mackman.co.uk>
2024-01-26 13:48:21 +0000

    always feels like starting from scratch

--- functions.php
+++ functions.php
@@ -89,3 +89,35 @@ function inception_register_block_styles() {
 	}
 }
 add_action( 'init', 'inception_register_block_styles' );
+
+
+/*
+ * Add the tracking snippets for the head in here
+ */
+function add_gtm_head() { ?>
+
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KM52M2F');</script>
+	<!-- End Google Tag Manager -->
+
+  <?php 
+}
+add_action( 'wp_head', 'add_gtm_head', 1 );
+
+/*
+ * Add the tracking snippets for the body in here
+ */
+function add_gtm_body() { ?>
+
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KM52M2F"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+
+  <?php
+}
+add_action( 'wp_body_open', 'add_gtm_body' );
